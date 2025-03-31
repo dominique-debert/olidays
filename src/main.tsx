603584167ee9867e141ff02060d20ada09f19f3f
Dominique DEBERT <dominiquedebert@gmail.com>
2025-03-31 18:43:05 +0200

    Error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error("Root element not found. Make sure the element with id 'root' exists in your HTML.")
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
