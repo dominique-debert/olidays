ca0dbd188ea0f38ff6bf2e4dca6bd1cc73d17195
Dominique DEBERT <dominiquedebert@gmail.com>
2025-04-01 09:28:58 +0200

    Add npm types

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Nécessaire pour GitHub Pages
+  base: process.env.BASE_URL, // Nécessaire pour GitHub Pages
 })
